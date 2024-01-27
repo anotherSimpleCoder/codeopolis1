1bc0b1a408e53a83f7a6a83d2634c8fda32840aa
Amsakan Bavan <amsakan.bavan@softwareag.com>
2024-01-27 18:39:02 +0100

    Cleanup

--- src/main/java/net/martinburger/sesqa/programming/Codeopolis.java
+++
@@ -1,8 +0,0 @@
-package net.martinburger.sesqa.programming;
-
-public class Codeopolis {
-	  public static void main(String[] args) {
-		  TextInterface textInterface = new TextInterface();
-		  textInterface.mainMenu();
-	  }
-}
